1cf67794023e638de47f1b1ee24da80560649385
zhohra-fatima <fatima.tuz.zhohra@gmail.com>
2024-07-08 14:04:44 +0800

    Finished welcome to Karel lesson

---
+++ Welcome/Welcome to Karel/Rainbow.py
@@ -0,0 +1,45 @@
+"""
+File: Rainbow.py
+------------------------------
+Karel makes a rainbow!
+"""
+
+"""
+This is a worked example. This code is starter code; you should edit and run it to
+solve the problem. You can click the blue show solution button on the left to see
+the answer if you get too stuck or want to check your work!
+
+Note: The unit test for this worked example is currently not working. Ignore it!
+"""
+
+from karel.stanfordkarel import *
+
+def main():
+    """
+    You should write your code to make Karel do its task in
+    this function. Make sure to delete the 'pass' line before
+    starting to write your own code. You should also delete this
+    comment and replace it with a better, more descriptive one.
+    """
+    pass  # Delete this line and write your code here! :)
+
+    paint_corner('Red')
+    move()
+
+    paint_corner('Orange')
+    move()
+
+    paint_corner('Yellow')
+    move()
+
+    paint_corner('Green')
+    move()
+
+    paint_corner('Blue')
+    move()
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
---
+++ Welcome/Welcome to Karel/step_up.py
@@ -0,0 +1,30 @@
+"""
+This is a worked example. This code is starter code; you should edit and run it to
+solve the problem. You can click the blue show solution button on the left to see
+the answer if you get too stuck or want to check your work!
+"""
+
+# This tells PyCharm who Karel is
+# Every Karel file has a line just like it
+from karel.stanfordkarel import *
+
+
+# this program executes in a special function called main
+def main():
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+
+
+# This is "boilerplate" code which launches your code
+# when you hit the run button
+if __name__ == '__main__':
+    main()
